1cedf3c4aba7c66167f8c2365fed74592be3b1b2
AmandaKly <105291593+AmandaKly@users.noreply.github.com>
2023-01-09 23:06:46 -0300

    Adicionando Api que será usada na aplicação

---
+++ recyclern/app/Controllers/Http/MateriaisController.ts
@@ -0,0 +1,38 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Material from 'App/Models/Material'
+import Ponto from 'App/Models/Ponto'
+export default class MateriaisController {
+   
+    public async index({ response }:HttpContextContract) {
+        const materiais = await Material.query()
+
+        return response.ok(materiais)
+    }
+
+
+    public async store({request, response, params}:HttpContextContract) {
+        const body = request.body()
+        const pontoId = params.id
+
+        await Ponto.findOrFail(pontoId)
+
+        const materiais = await Material.create(body)
+
+        response.status(201)
+
+        return {
+            message: "Material adicionado com sucesso",
+            data: materiais,
+        }
+    }
+
+    public async destroy({params, response}:HttpContextContract) {
+        const materiais = await Material.findOrFail(params.id)
+        try {
+            await materiais.delete()
+            return response.ok(materiais)
+        } catch (e) {
+            return response.badRequest(e)
+        }
+    }
+}
